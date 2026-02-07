57286b6ea2ba57ab3a167ffb82b77361d279762c
pankajgour12 <pankajgour5000@gmail.com>
2026-02-07 21:25:28 +0530

    section 1 done

--- src/components/HomeClient.tsx
+++ src/components/HomeClient.tsx
@@ -9,6 +9,23 @@ const HomeClient = ({ email }: { email: string }) => {
     window.location.href = "/api/auth/login";
   };
 
+   const features=[
+    {
+      title:"Easy Integration",
+      description:"Integrate our AI chatbot seamlessly into your website with just a few lines of code. No technical expertise required, making it accessible for businesses of all sizes."
+    },
+    {
+      title:"Admin Controlled",
+      description:"ake full control of your chatbot's knowledge base. Easily add, edit, or remove information to ensure your customers receive accurate and up-to-date answers."
+    },
+    {
+      title:"Instant Customer Support",
+      description:"Provide your customers with instant answers to their queries, improving their experience and increasing satisfaction. Our AI chatbot is available 24/7 to assist your customers whenever they need."
+    }
+
+
+    ]
+
   return (
     <div className="min-h-screen w-full relative z-10 overflow-hidden">
      
@@ -32,7 +49,7 @@ const HomeClient = ({ email }: { email: string }) => {
       <div className="min-h-screen text-amber-50">
         <Navbar email={email} />
 
-        <section className="pt-32 pb-24 px-6">
+        <section className="pt-36 pb-32 px-6">
           <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-14 lg:gap-24 items-center">
             
             {/* LEFT */}
@@ -41,7 +58,7 @@ const HomeClient = ({ email }: { email: string }) => {
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.7 }}
             >
-              <h1 className="text-3xl sm:text-4xl lg:text-5xl font-semibold leading-tight">
+              <h1 className="text-4xl sm:text-4xl lg:text-5xl font-semibold leading-tight">
                 AI Customer Support Chatbot <br />
                 <span className="text-gray-300 text-xl sm:text-2xl lg:text-3xl font-bold">
                   Build for Modern Websites
@@ -59,7 +76,7 @@ const HomeClient = ({ email }: { email: string }) => {
                     Go to Dashboard
                   </button>
                 ) : (
-                  <button
+                  <button 
                     className="px-7 py-3 rounded-xl bg-gradient-to-br from-gray-600 to-amber-500/30 text-white font-semibold shadow-lg hover:scale-105 transition-all duration-300 backdrop-blur-md border border-white/10"
                     onClick={handleLogin}
                   >
@@ -67,9 +84,9 @@ const HomeClient = ({ email }: { email: string }) => {
                   </button>
                 )}
 
-                <button className="px-7 py-3 rounded-xl border border-gray-600 text-white font-semibold hover:bg-gray-800 hover:scale-105 transition-all duration-300 backdrop-blur-md">
+                <a href="#feature" className="px-7 py-3 rounded-xl border border-gray-600 text-white font-semibold hover:bg-gray-800 hover:scale-105 transition-all duration-300 backdrop-blur-md">
                   Learn More
-                </button>
+                </a>
               </div>
             </motion.div>
 
@@ -107,6 +124,35 @@ const HomeClient = ({ email }: { email: string }) => {
             </motion.div>
           </div>
         </section>
+
+        <section
+        id="feature"
+        className=" py-28 px-6 bg-gradient-to-br from-black/20 to-zinc-900/10   border-t border-white/20 " 
+        >
+          <div className="max-w-6xl mx-auto">
+
+          <motion.h2
+          initial={{ opacity: 0, y: 20 }}
+          whileInView={{ opacity: 1, y: 0}}
+          viewport={{once:false}}
+          transition={{ duration: 0.7 }}
+          className="text-3xl sm:text-3xl lg:text-4xl font-semibold text-center"> 
+            Why Choose Our AI Chatbot?
+          </motion.h2>
+            
+            <div className="mt-16 grid grid-cols-1 md:grid-cols-3 gap-10 " >
+            
+
+
+
+            </div>
+
+          </div>
+
+        </section>
+
+
+
       </div>
     </div>
   );
